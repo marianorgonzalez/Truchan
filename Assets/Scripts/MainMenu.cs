9e9a2d2536b1a1ac3d66df5f395680d53012dbdb
lisandroArosBouvet <73535661+lisandroArosBouvet@users.noreply.github.com>
2026-08-31 11:42:17 -0300

    Se acomodo y corrigio las animaciones

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,17 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-  [SerializeField] GameObject buttonContainer;
+    [SerializeField] GameObject[] buttonContainer;
+    bool _busy = false;
 
-  public void GoToMainScene()
-  {
-    buttonContainer.SetActive(false);
-    StartCoroutine(GoToMainSceneCoroutine());
-  }
+    public void GoToMainScene(string sceneName)
+    {
+        if (_busy) return;
+        foreach (var btn in buttonContainer)
+        {
+            btn.SetActive(false);
+        }
+        StartCoroutine(GoToMainSceneCoroutine(sceneName));
+    }
 
-  private IEnumerator GoToMainSceneCoroutine()
-  {
-    yield return new WaitForSeconds(1f);
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-  }
+    private IEnumerator GoToMainSceneCoroutine(string sceneName)
+    {
+        _busy = true;
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
 }
